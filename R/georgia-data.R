4b94a94d35eaaafb742ff54c1bbc4a2cc4a34017
Juliana Rosa <47371910+julianamrosa@users.noreply.github.com>
2024-02-13 22:16:52 -0300

    Add files via upload

---
+++ R/georgia-data.R
@@ -0,0 +1,25 @@
+#' Georgia dataset
+#'
+#' The Georgia census data set from Fotheringham et al. (2002) in shapefile format.
+#' 
+#' @docType data
+#' 
+#' @usage data(georgia)
+#' 
+#' @format A data frame with with 159 observations on the following 13 variables:
+#' \itemize{
+#' \item \code{AreaKey} - a numeric vector
+#' \item \code{Latitude} - a numeric vector
+#' \item \code{Longitud} - a numeric vector
+#' \item \code{TotPop90} - a numeric vector
+#' \item \code{PctRural} - a numeric vector
+#' \item \code{PctBach} - a numeric vector
+#' \item \code{PctEld} - a numeric vector
+#' \item \code{PctFB} - a numeric vector
+#' \item \code{PctPov} - a numeric vector
+#' \item \code{PctBlack} - a numeric vector
+#' \item \code{ID} - a numeric vector
+#' \item \code{X} - a numeric vector
+#' \item \code{Y} - a numeric vector
+#' }
+"georgia"
